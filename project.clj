34788d98f0925673bad4b59d11ff5c7853bc26a5
Miikka Koskinen <miikka.koskinen@iki.fi>
2018-08-12 22:08:20 +0300

    chore: configure semantic-release

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/miikka/clj-sem-rel-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-set-version "0.4.1"]])
